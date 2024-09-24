32f01b694637ade35b8887c45d455cbdf3f5eae7
Nightmare <fajardoariel435@gmail.com>
2024-09-24 21:07:00 +0200

    Options collab

--- api/index.js
+++ api/index.js
@@ -10,8 +10,20 @@ const PORT = process.env.PORT ?? 3000
 
 
 app.use(cors({
+    origin: "https://tier-list-frontend.vercel.app",
     credentials: true
 }))
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', "https://tier-list-frontend.vercel.app")
+    res.header('Access-Control-Allow-Credentials', 'true')
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE')
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+    if (req.method === 'OPTIONS') {
+        return res.sendStatus(200)
+    }
+    next()
+})
 
 app.use(cookieParser())
 app.use(AuthController.authorize)
